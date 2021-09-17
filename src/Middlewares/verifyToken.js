16290403ebebb8c350e737ec2f8a875157291df7
Gianpierre Galvez Wizeline <gianpierre.galvez@wizeline.com>
2021-09-17 15:14:01 -0500

    Cleaning

--- src/Middlewares/verifyToken.js
+++ src/Middlewares/verifyToken.js
@@ -6,33 +6,32 @@ const jwt = require("jsonwebtoken");
 const unauthorizedStatus = 401;
 
 const verifyToken = async (req, res, next) => {
-	const token = req.headers["authorization"];
+    const token = req.headers["authorization"];
 
-	if (!token)
-		return res.status(unauthorizedStatus).json({
-			message: `No token provided`,
-		});
-	try {
-		const decoded = jwt.verify(token, process.env.SECRET_TOKEN);
-		// return res.status(200).json({ decoded });
-		req.user = decoded.id;
+    if (!token)
+        return res.status(unauthorizedStatus).json({
+            message: `No token provided`,
+        });
+    try {
+        const decoded = jwt.verify(token, process.env.SECRET_TOKEN);
+        req.user = decoded.id;
 
-		// const user = await User.findByPk(req.userId)
-		// 	.then((data) => {
-		// 		if (data) {
-		// 			return data.dataValues;
-		// 		}
-		// 	})
-		// 	.catch((err) => {
-		// 		return res.status(404).json({ message: `${err.message} token` });
-		// 	});
+        // const user = await User.findByPk(req.userId)
+        // 	.then((data) => {
+        // 		if (data) {
+        // 			return data.dataValues;
+        // 		}
+        // 	})
+        // 	.catch((err) => {
+        // 		return res.status(404).json({ message: `${err.message} token` });
+        // 	});
 
-		// if (!user) return res.status(404).json({ message: "No user found" });
+        // if (!user) return res.status(404).json({ message: "No user found" });
 
-		next();
-	} catch (error) {
-		return res.status(401).json({ message: `${error} ${token}` });
-	}
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: `${error} ${token}` });
+    }
 };
 
 module.exports.verifyToken = verifyToken;
